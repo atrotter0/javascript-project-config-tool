63bfb08cef7b5cb2617b3736e2be5500b0b597c7
Abel Trotter <a_trotter0@hotmail.com>
2018-08-06 21:26:55 -0700

    Add .env to gitignore, add dotenv-webpack dependency

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -20,7 +21,8 @@ module.exports = {
       title: 'Journal Assignment',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
