d0149aaa372e144b2d49069275e3610abeef4191
Abel Trotter <a_trotter0@hotmail.com>
2018-10-02 18:39:03 -0700

    Update project title in html plugin

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Journal Assignment',
+      title: 'Project Title',
       template: './src/index.html',
       inject: 'body'
     }),
